33894a19a03cdc78294a67ea3f9d26d78d080cb6
maki <komirimaha@icloud.com>
2023-02-16 21:13:54 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const pullDownButton = document.getElementById("lists")
 
+const pullDownParents = document.getElementById("pull-down")
+
 pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
+    this.setAttribute("style", "background-color:blue;")
 })
 
 pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:red;")
+    this.removeAttribute("style", "background-color:red;")
 })
 
 pullDownButton.addEventListener('click', function(){
-    pullDownButton.setAttribute("style", "background-color:green;")
+    this.setAttribute("style", "background-color:green;");
+    if (pullDownParents.getAttribute("style") =="display:block;"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style", "display:block;")
+        console.log("表示")
+    }
 })
