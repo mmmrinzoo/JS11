772490616ada941bab338d979fa7e9d7d809fcf1
maki <komirimaha@icloud.com>
2023-02-16 20:55:06 +0900

    HTML JS CSS構文追加
    index.html
    script.js
    style.css

---
+++ script.js
@@ -0,0 +1,13 @@
+const pullDownButton = document.getElementById("lists")
+
+pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+})
+
+pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "background-color:red;")
+})
+
+pullDownButton.addEventListener('click', function(){
+    pullDownButton.setAttribute("style", "background-color:green;")
+})
